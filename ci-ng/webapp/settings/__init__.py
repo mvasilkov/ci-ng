28e312ceacfcd110df6dafbd560b9695daa28295
Mark Vasilkov <mvasilkov@gmail.com>
2016-02-11 12:03:58 +0200

    Initial commit

---
+++ ci-ng/webapp/settings/__init__.py
@@ -0,0 +1,9 @@
+'Debug settings'
+
+from .base import *
+
+ALLOWED_HOSTS = ()
+
+DEBUG = True
+
+SECRET_KEY = 'x86-64 (also known as x64, x86_64 and AMD64) is the 64-bit version of the x86 instruction set.'
---
+++ ci-ng/webapp/settings/base.py
@@ -0,0 +1,71 @@
+import os.path as op
+
+PATH = op.dirname(op.dirname(op.abspath(__file__)))
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'webapp.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'webapp.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': op.join(PATH, 'db.sqlite3'),
+    }
+}
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
+
+TIME_ZONE = 'Israel'
+
+USE_I18N = USE_L10N = USE_TZ = False
+
+
+# Static files
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = '/static/'
